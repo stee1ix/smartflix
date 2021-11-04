11c3ad120e209dd26c752e49d6c91213e54512e3
stee1ix <rituraj.ranjan.42@gmail.com>
2021-11-04 19:27:13 +0530

    logo animation

---
+++ script.js
@@ -0,0 +1,6 @@
+const logo = document.querySelectorAll('#logo path');
+
+// for (let index = 0; index < logo.length; index++) {
+// 	const length = logo[index].getTotalLength();
+// 	// console.log(`${index + 1} is ${length}`);
+// }
