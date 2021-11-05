ce52444342f5da8475fdb9635ba30afa4800fef4
stee1ix <rituraj.ranjan.42@gmail.com>
2021-11-05 22:23:25 +0530

    intro slider animation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const logo = document.querySelectorAll('#logo path');
+// const logo = document.querySelectorAll('#logo path');
 
 // for (let index = 0; index < logo.length; index++) {
 // 	const length = logo[index].getTotalLength();
-// 	// console.log(`${index + 1} is ${length}`);
+//  console.log(`${index + 1} is ${length}`);
 // }
+
+const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
+
+timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+
+timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+timeline.fromTo(
+	'.big-text',
+	{ opacity: 0 },
+	{ opacity: 1, duration: 1 },
+	'-=1'
+);
