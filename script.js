b6671663b1fc0709b681cc45c2b53ef80e14a20d
stee1ix <rituraj.ranjan.42@gmail.com>
2021-11-15 12:54:01 +0530

    login dialog added

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const logo = document.querySelectorAll('#logo path');
+// const logo = document.querySelectorAll('#logo path');
 
-for (let index = 0; index < logo.length; index++) {
-	const length = logo[index].getTotalLength();
-	console.log(`${index + 1} is ${length}`);
-}
+// for (let index = 0; index < logo.length; index++) {
+// 	const length = logo[index].getTotalLength();
+// 	console.log(`${index + 1} is ${length}`);
+// }
 
-const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
+// const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
+// timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
 
-timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+// timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
 
-timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
-timeline.fromTo(
-	'.big-text-wrapper',
-	{ opacity: 0 },
-	{ opacity: 1, duration: 1 },
-	'-=1'
-);
+// timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+// timeline.fromTo(
+// 	'.big-text-wrapper',
+// 	{ opacity: 0 },
+// 	{ opacity: 1, duration: 1 },
+// 	'-=1'
+// );
 
 const desp1 = document.querySelector('#desp1');
 const doraemon1 = document.querySelector('#doraemon1');
@@ -229,4 +229,16 @@ function ronaldoClose() {
 }
 function doraemonClose() {
 	document.querySelector('#doraemon').remove();
+}
+
+function showLogin() {
+	const login = document.querySelector('.login');
+	login.style.opacity = 1;
+	document.querySelector('#login-close').style.display = 'block';
+}
+
+function closeLogin() {
+	const login = document.querySelector('.login');
+	login.style.opacity = 0;
+	document.querySelector('#login-close').style.display = 'none';
 }
