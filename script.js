90ff7881c5a159e7df131bf18aa0c27f71ad2dc4
stee1ix <rituraj.ranjan.42@gmail.com>
2021-11-15 10:49:19 +0530

    added movie and about

--- script.js
+++ script.js
@@ -5,16 +5,151 @@
 //  console.log(`${index + 1} is ${length}`);
 // }
 
-const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
+// const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
+// timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
 
-timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+// timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
 
-timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
-timeline.fromTo(
-	'.big-text',
-	{ opacity: 0 },
-	{ opacity: 1, duration: 1 },
-	'-=1'
-);
+// timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+// timeline.fromTo(
+// 	'.big-text-wrapper',
+// 	{ opacity: 0 },
+// 	{ opacity: 1, duration: 1 },
+// 	'-=1'
+// );
+
+const desp1 = document.querySelector('#desp1');
+const doraemon1 = document.querySelector('#doraemon1');
+const interstellar1 = document.querySelector('#interstellar1');
+const msec1 = document.querySelector('#msec1');
+const ronaldo1 = document.querySelector('#ronaldo1');
+const suits1 = document.querySelector('#suits1');
+const ucl1 = document.querySelector('#ucl1');
+const tot1 = document.querySelector('#tot1');
+const mancity1 = document.querySelector('#mancity1');
+const batman1 = document.querySelector('#batman1');
+const desp = document.querySelector('#desp');
+const doraemon = document.querySelector('#doraemon');
+const interstellar = document.querySelector('#interstellar');
+const msec = document.querySelector('#msec');
+const ronaldo = document.querySelector('#ronaldo');
+const suits = document.querySelector('#suits');
+const ucl = document.querySelector('#ucl');
+const tot = document.querySelector('#tot');
+const mancity = document.querySelector('#mancity');
+const batman = document.querySelector('#batman');
+
+const close1 = document.querySelector('#close1');
+const close2 = document.querySelector('#close2');
+const close3 = document.querySelector('#close3');
+const close4 = document.querySelector('#close4');
+const close5 = document.querySelector('#close5');
+const close6 = document.querySelector('#close6');
+const close7 = document.querySelector('#close7');
+const close8 = document.querySelector('#close8');
+const close9 = document.querySelector('#close9');
+const close10 = document.querySelector('#close10');
+
+close1.addEventListener('click', () => {
+	desp.style.opacity = 0;
+	desp.style['z-index'] = -1;
+	desp.remove();
+});
+
+desp1.addEventListener('click', () => {
+	desp.style.opacity = 1;
+	desp.style['z-index'] = 2;
+	desp.a;
+});
+
+close2.addEventListener('click', () => {
+	doraemon.style.opacity = 0;
+	doraemon.style['z-index'] = -1;
+	doraemon.remove();
+});
+
+doraemon1.addEventListener('click', () => {
+	doraemon.style.opacity = 1;
+	doraemon.style['z-index'] = 2;
+});
+close3.addEventListener('click', () => {
+	interstellar.style.opacity = 0;
+	interstellar.style['z-index'] = -1;
+	interstellar.remove();
+});
+
+interstellar1.addEventListener('click', () => {
+	interstellar.style.opacity = 1;
+	interstellar.style['z-index'] = 2;
+});
+close4.addEventListener('click', () => {
+	msec.style.opacity = 0;
+	msec.style['z-index'] = -1;
+	msec.remove();
+});
+
+msec1.addEventListener('click', () => {
+	msec.style.opacity = 1;
+	msec.style['z-index'] = 2;
+});
+close5.addEventListener('click', () => {
+	ronaldo.style.opacity = 0;
+	ronaldo.style['z-index'] = -1;
+	ronaldo.remove();
+});
+
+ronaldo1.addEventListener('click', () => {
+	ronaldo.style.opacity = 1;
+	ronaldo.style['z-index'] = 2;
+});
+close6.addEventListener('click', () => {
+	suits.style.opacity = 0;
+	suits.style['z-index'] = -1;
+	suits.remove();
+});
+
+suits1.addEventListener('click', () => {
+	suits.style.opacity = 1;
+	suits.style['z-index'] = 2;
+});
+close7.addEventListener('click', () => {
+	ucl.style.opacity = 0;
+	ucl.style['z-index'] = -1;
+	ucl.remove();
+});
+
+ucl1.addEventListener('click', () => {
+	ucl.style.opacity = 1;
+	ucl.style['z-index'] = 2;
+});
+close8.addEventListener('click', () => {
+	tot.style.opacity = 0;
+	tot.style['z-index'] = -1;
+	tot.remove();
+});
+
+tot1.addEventListener('click', () => {
+	tot.style.opacity = 1;
+	tot.style['z-index'] = 2;
+});
+close9.addEventListener('click', () => {
+	mancity.style.opacity = 0;
+	mancity.style['z-index'] = -1;
+	mancity.remove();
+});
+
+mancity1.addEventListener('click', () => {
+	mancity.style.opacity = 1;
+	mancity.style['z-index'] = 2;
+});
+close10.addEventListener('click', () => {
+	batman.style.opacity = 0;
+	batman.style['z-index'] = -1;
+	batman.remove();
+});
+
+batman1.addEventListener('click', () => {
+	batman.style.opacity = 1;
+	batman.style['z-index'] = 2;
+});
