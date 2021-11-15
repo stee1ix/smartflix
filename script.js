b15d844160ea088814e451f683f1034cedefb949
stee1ix <rituraj.ranjan.42@gmail.com>
2021-11-15 12:55:10 +0530

    resumed loading animation

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// const logo = document.querySelectorAll('#logo path');
+const logo = document.querySelectorAll('#logo path');
 
-// for (let index = 0; index < logo.length; index++) {
-// 	const length = logo[index].getTotalLength();
-// 	console.log(`${index + 1} is ${length}`);
-// }
+for (let index = 0; index < logo.length; index++) {
+	const length = logo[index].getTotalLength();
+	console.log(`${index + 1} is ${length}`);
+}
 
-// const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
+const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-// timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
+timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 4.7 });
 
-// timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
 
-// timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
-// timeline.fromTo(
-// 	'.big-text-wrapper',
-// 	{ opacity: 0 },
-// 	{ opacity: 1, duration: 1 },
-// 	'-=1'
-// );
+timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+timeline.fromTo(
+	'.big-text-wrapper',
+	{ opacity: 0 },
+	{ opacity: 1, duration: 1 },
+	'-=1'
+);
 
 const desp1 = document.querySelector('#desp1');
 const doraemon1 = document.querySelector('#doraemon1');
